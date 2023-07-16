934897bcc455b2fb0d038ef6179207799e541460
Andrew McWatters <mcwattersandrew@gmail.com>
2023-07-15 22:34:03 -0700

    Create UITabBar.js

---
+++ javascripts/UITabBar.js
@@ -0,0 +1,59 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements
+// https://developer.apple.com/documentation/uikit/uitabbar
+// Create a class for the element
+class UITabBar extends UIView {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // Create a shadow root
+    /* const shadow = */ this.attachShadow({mode: 'open'});
+    this.draw();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.draw();
+  }
+
+  draw() {
+    const shadow = this.shadowRoot;
+
+    shadow.innerHTML = 
+`<style>
+  #background {
+    /* --padding-bottom: 34px; */
+    /* padding-bottom: var(--padding-bottom); */
+    padding-bottom: env(safe-area-inset-bottom);
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    /* min-height: calc(83px - var(--padding-bottom)); */
+    min-height: 44px;
+    background: rgba(247,247,247,0.80);
+    box-shadow: 0 0 0 0 rgba(0,0,0,0.30);
+  }
+
+  @media (prefers-color-scheme: dark) {
+    #background {
+      background: rgba(22,22,22,0.80);
+      box-shadow: 0 0 0 0 rgba(255,255,255,0.16);
+    }
+  }
+
+  #background > div {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+</style>
+<div id="background">
+  <div>
+    <slot></slot>
+  </div>
+</div>`;
+  }
+}
+
+// Define the new element
+customElements.define('x-uitabbar', UITabBar);
---
+++ javascripts/UITabBarItem.js
@@ -0,0 +1,70 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements
+// https://developer.apple.com/documentation/uikit/uitabbaritem
+// Create a class for the element
+class UITabBarItem extends UIView {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // Create a shadow root
+    /* const shadow = */ this.attachShadow({mode: 'open'});
+    this.draw();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.draw();
+  }
+
+  draw() {
+    const shadow = this.shadowRoot;
+
+    shadow.innerHTML = 
+`<style>
+  #root {
+    min-height: 48px;
+    min-height: 49px;
+  }
+
+  #symbol {
+    margin-top: 7px;
+    /* font-family: SFPro-Medium; */
+    font-weight: 500;
+    font-size: 18px;
+    color: #000000;
+    text-align: center;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    #symbol {
+      color: #FFFFFF;
+    }
+  }
+
+  #label {
+    margin-bottom: 2px;
+    /* font-family: SFPro-Medium; */
+    font-weight: 500;
+    font-size: 10px;
+    color: #000000;
+    text-align: center;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    #label {
+      color: #FFFFFF;
+    }
+  }
+</style>
+<div id="root">
+  <div id="symbol">
+    ★
+  </div>
+  <div id="label">
+    Tab
+  </div>
+</div>`;
+  }
+}
+
+// Define the new element
+customElements.define('x-uitabbaritem', UITabBarItem);
