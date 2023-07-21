fda31c89e78be738716d8f445a5735e211be7dea
Andrew McWatters <mcwattersandrew@gmail.com>
2023-07-21 14:45:25 -0700

    Create UIBarItem.js

---
+++ javascripts/UIBarItem.js
@@ -0,0 +1,21 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements
+// https://developer.apple.com/documentation/uikit/uibaritem
+class UIBarItem extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // Create a shadow root
+    /* const shadow = */ this.attachShadow({mode: 'open'});
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.draw();
+  }
+
+  connectedCallback() {
+    this.draw();
+  }
+
+  draw() {}
+}
