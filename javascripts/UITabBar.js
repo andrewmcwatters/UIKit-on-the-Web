9e7a55b4520ee62193c5a9a688d2a2a2b8b84774
Andrew McWatters <mcwattersandrew@gmail.com>
2023-08-13 20:14:34 -0700

    Fade out background, box-shadow, and backdrop-filter if we scroll past the view

--- javascripts/UITabBar.js
+++ javascripts/UITabBar.js
@@ -2,9 +2,76 @@
 // https://developer.apple.com/documentation/uikit/uitabbar
 // Create a class for the element
 class UITabBar extends UIView {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#improving_scrolling_performance_with_passive_listeners
+    /* Feature detection */
+    let passiveIfSupported = false;
+
+    try {
+      window.addEventListener(
+        "test",
+        null,
+        Object.defineProperty({}, "passive", {
+          get() {
+            passiveIfSupported = { passive: true };
+          },
+        }),
+      );
+    } catch (err) {}
+
+    // handleScroll
+    this.handleScroll = this.handleScroll.bind(this);
+    document.addEventListener('scroll', this.handleScroll, passiveIfSupported);
+  }
+
   connectedCallback() {
     this.draw();
     this.initializeStyles();
+  }
+
+  handleScroll(event) {
+    // Fade out background, box-shadow, and backdrop-filter if we scroll past
+    // the view
+    //
+    // See also: UINavigationBar.handleScroll()
+    const view       = document.querySelector('apple-uiview');
+    const viewStyle  = getComputedStyle(view);
+    const background = this.shadowRoot.querySelector('#background');
+
+    const bottom     = scrollY + innerHeight;
+    let offsetHeight = view.offsetHeight;
+    offsetHeight     = offsetHeight + parseFloat(viewStyle.marginBottom);
+
+    if (bottom >= offsetHeight - 9) {
+      // offsetHeight   = offsetHeight + 9;
+      // const clamp    = (n, min, max) => Math.max(min, Math.min(n, max));
+      // const percent  = 1 - clamp((bottom - offsetHeight + 9) / 9, 0, 1);
+      const percent  = 1 - Math.max(0, Math.min((bottom - offsetHeight + 9) / 9, 1));
+
+      background.style.background = '';
+      const backgroundStyle       = getComputedStyle(background);
+      const backgroundColor       = backgroundStyle
+        .backgroundColor
+        .replace('0.8',  `calc(0.8  * ${percent})`);
+      background.style.background = backgroundColor;
+
+      background.style.boxShadow  = '';
+      let boxShadow = backgroundStyle
+        .boxShadow
+        .replace('0.3',  `calc(0.3  * ${percent})`);
+      boxShadow = backgroundStyle
+        .boxShadow
+        .replace('0.16', `calc(0.16 * ${percent})`);
+      background.style.boxShadow  = boxShadow;
+    } else {
+      background.style.background           = '';
+      background.style.boxShadow            = '';
+      background.style.backdropFilter       = '';
+      background.style.webkitBackdropFilter = '';
+    }
   }
 
   initializeStyles() {
@@ -15,6 +82,11 @@ class UITabBar extends UIView {
     background.style.boxShadow            = 'none';
     background.style.backdropFilter       = 'none';
     background.style.webkitBackdropFilter = 'none';
+
+    requestAnimationFrame(() => {
+      const view = document.querySelector('apple-uiview');
+      view.style.marginBottom = background.offsetHeight + 'px';
+    });
   }
 
   draw() {
